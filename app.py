5f185f925eed79972d73ecb533d6f0311d3df752
Lincoln Kermit <104798220+LincolnKermit@users.noreply.github.com>
2024-09-10 23:04:42 +0200

    Update files and About page created

--- app.py
+++ app.py
@@ -6,12 +6,16 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
+    print("Log: Index page from"+request.remote_addr)
     return render_template('index.html')
 
 @app.route('/terminal')
 def terminal():
     return render_template('terminal.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
