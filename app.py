d73574aab896bb6122cbf261afdbbc150a3192a9
TreTrenoff <85366443+TreTrenoff@users.noreply.github.com>
2024-09-11 10:17:40 +0200

    Merging

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
-import datetime
-from flask import Flask, render_template, url_for, request, redirect
-import os
-
-from model import Homework, db
+from flask import Flask, render_template, request, redirect, url_for
+from db import db
+from model import Homework
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///homework.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)  # Initialiser db avec l'application Flask
 
 @app.route('/')
 @app.route('/index')
 def index():
-    print("Log: Index page from"+request.remote_addr)
-    return render_template('index.html')
+    print("Log: Index page from " + request.remote_addr)
+    homeworks = Homework.query.all()
+    return render_template('index.html', homeworks=homeworks)
 
 @app.route('/terminal')
 def terminal():
@@ -22,16 +26,17 @@ def about():
 
 @app.route('/learning_redirect')
 def learning_redirect():
-    return render_template('/learning/learning_main.html')
+    return render_template('learning/learning_main.html')
 
 @app.route('/add_homework', methods=['GET', 'POST'])
 def add_homework():
     if request.method == 'POST':
         date_str = request.form['date']
         homework_text = request.form['homework']
+        matiere = request.form['matiere']
         date = datetime.strptime(date_str, '%Y-%m-%d').date()
         
-        new_homework = Homework(date=date, text=homework_text)
+        new_homework = Homework(date=date, text=homework_text, matiere=matiere)
         db.session.add(new_homework)
         db.session.commit()
         
@@ -39,5 +44,16 @@ def add_homework():
 
     return render_template('add_homework.html')
 
+
+@app.route("/learning/c++")
+def learning_cpp():
+    return render_template('/learning/c++/Cc++.html')
+
+@app.route("/learning/c++/Lesbases")
+def learning_cpp_Lesbases():
+    return render_template('/learning/c++/Lesbases.html')
+
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()  # Crée les tables dans la base de données
     app.run(debug=True)
--- model.py
+++ model.py
@@ -1,9 +1,18 @@
-from app import db
+from db import db
+
+# Liste des matières prédéfinies
+MATIERE_CHOICES = [
+    ('physique', 'Physique'),
+    ('math', 'Math'),
+    ('ir', 'IR'),
+    ('culture', 'Culture'),
+]
 
 class Homework(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date, nullable=False)
     text = db.Column(db.String(255), nullable=False)
+    matiere = db.Column(db.String(50), nullable=False)  # Champ de matière
 
     def __repr__(self):
-        return f'<Homework {self.date} - {self.text}>'
+        return f'<Homework {self.date} - {self.text} - {self.matiere}>'
